79cda78e039030108eed3d9fcb655bd05d435cef
root <root@localhost.localdomain>
2013-09-25 22:38:21 +0200

    Updated startscripts

---
+++ start/01-copy-sshkeys.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ../ssh_certs/id_rsa* ~/.ssh/
+
+chmod 600 ~/.ssh/id_rsa
--- start/04-chef_repo.sh
+++ start/04-chef_repo.sh
@@ -2,6 +2,6 @@
 
 # see http://beacon.wharton.upenn.edu/404/2010/12/chef-out-of-the-gate/
 
+cd ..
 git clone git@github.com:Waxolunist/chef-repo.git repo
 cd repo
-mkdir -p site-cookbooks
